9a9ff28e2ae65b547949d12e764ebc0ebecbd1f4
DevExpressExampleBot <fakeEmail>
2023-06-19 14:32:22 +0400

    Source auto update [skip ci]

---
+++ VB/App_Code/CustomExceptions.vb
@@ -0,0 +1,12 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Namespace CustomExceptions
+	Public Class MyException
+		Inherits Exception
+
+		Public Sub New(ByVal message As String)
+			MyBase.New(message)
+		End Sub
+	End Class
+End Namespace
